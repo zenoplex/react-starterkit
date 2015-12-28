ed2cad8f825f4011a1c0f5c059c1472d8df35d2e
Gen Ichino <zenoplex@gmail.com>
2015-12-28 15:48:08 +0900

    Fixed Counter spec

--- test/components/Counter_spec.js
+++ test/components/Counter_spec.js
@@ -10,7 +10,7 @@ describe('Counter', () => {
   });
 
   it('should render specified `increment`', () => {
-    const wrapper = shallow(<Counter increment="666"/>);
+    const wrapper = shallow(<Counter increment={666}/>);
     assert(/666/.test(wrapper.find('h1').text()));
   });
 
@@ -18,7 +18,7 @@ describe('Counter', () => {
     const wrapper = mount(<Counter increment={500}/>);
 
     setTimeout(() => {
-      console.log(wrapper.find('h1').find('span').text() === 1000);
+      assert(Number(wrapper.find('h1').find('span').text()) === 1000);
       done();
     }, 1000);
   });
