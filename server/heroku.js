7aa56a245d8b998085f232e66e34335770bdc368
Gen Ichino <zenoplex@gmail.com>
2017-03-23 15:28:56 +0900

    fix: dynamic port number

--- server/heroku.js
+++ server/heroku.js
@@ -5,12 +5,12 @@ import { gzip, staticRoute, listen, browserHistory } from './middlewares';
 import config from '../webpack.config';
 
 const app = express();
+const port = Number(process.env.PORT) || 3000;
 
 R.pipe(
   gzip,
   staticRoute(config.output.path),
   browserHistory,
   staticRoute(config.output.path),
-  listen(3000),
+  listen(port),
 )(app);
-
